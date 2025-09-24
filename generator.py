de9ecc9e030a57fcd2232d775b675c94b986ae10
Stephen Yang <stephen.cr.yang@gmail.com>
2025-09-23 23:54:59 -0700

    Started research and begin prototyping

---
+++ generator.py
@@ -0,0 +1,7 @@
+def image_generate():
+    # Try using PIL?
+    # https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil
+    # It might not be perfect (not really random, etc)
+    # A small starting point at least
+    
+    return
