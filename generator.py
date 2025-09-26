30df445e9215998fc15ca058cc0c28438b996886
Stephen Yang <stephen.cr.yang@gmail.com>
2025-09-26 00:57:48 -0700

    Started basic work with the PIL image library

--- generator.py
+++ generator.py
@@ -1,7 +1,25 @@
-def image_generate():
-    # Try using PIL?
-    # https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil
-    # It might not be perfect (not really random, etc)
-    # A small starting point at least
+from PIL import Image, ImageDraw, ImageFont
+# Try using PIL?
+# https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil
+# It might not be perfect (not really random, etc)
+# A small starting point at least
+
+# https://stackoverflow.com/questions/64185819/pillow-draw-text-on-image-clear-between-iterations-python
+# Clearing text when we are "randomly creating"
+
+def image_generate(message, font, fontColor, createRandom = False, randomAmount = 0):
+
+    W, H = 250, 250
+    size = (W, H)
+    image = Image.new('RGB', size, 'white')
+    # TODO: First draw the text (any digit) in the middle of the screen
+    # Need to either implement a random position here or add start X and Y into another function
     
     return
+
+# Start big, resize using this code
+# https://stackoverflow.com/questions/42516212/how-to-save-a-resized-image-in-python
+def resize_img(img, new_w):
+    new_img = img.resize((new_w, new_w), Image.ANTIALIAS)
+    return new_img
+
